cf26f7f2c0eae11cbfaf0c9401874e6fb46370a8
Abdu <abdukr5@gmail.com.com>
2020-11-20 14:50:38 -0600

    Team member information prompted saved and displayed

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
+            TeamMember hMember = new TeamMember();
+            Console.WriteLine("Enter a team member's name");
+            hMember.Name = Console.ReadLine();
+
+
+            Console.WriteLine("Enter team member's skill level");
+            int enteredSkillLevel = int.Parse(Console.ReadLine());
+
+            if (enteredSkillLevel > 0)
+            {
+                hMember.SkillLevel = enteredSkillLevel;
+
+            }
+            else
+            {
+                Console.WriteLine("Enter Positive intiger number");
+
+            }
+
+            Console.WriteLine("Enter a team member's courage factor");
+            double enteredCourageFactor = double.Parse(Console.ReadLine());
+
+            if (enteredCourageFactor >= 0.0 && enteredCourageFactor <= 2.0)
+            {
+                hMember.CourageFactor = enteredCourageFactor;
+
+            }
+            else
+            {
+                Console.WriteLine("Enter numbers between 0.0 and 2.0");
+
+            }
+
+            Console.WriteLine($@"
+                    Name: {hMember.Name}
+             Skill Level: {hMember.SkillLevel}
+          Courage Factor: {hMember.CourageFactor}
+
+            ");
+        }
+    }
+}
+
---
+++ TeamMember.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Heist
+{
+    public class TeamMember
+    {
+
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public double CourageFactor { get; set; }
+
+    }
+}
